eba80128cc8728f6d64fec116ff1408c7fd4f5a1
prorooter007 <shreyashwasnik112@gmail.com>
2021-06-19 20:04:35 +0530

    AK3: Sweet-ify
    Signed-off-by: prorooter007 <shreyashwasnik112@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -10,14 +10,15 @@ do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=1
-device.name1=tissot
-supported.versions=9
-supported.patchlevels=
+device.name1=sweet
+device.name2=sweetin
+supported.versions=11
+supported.patchlevels=2019-06 -
 '; } # end properties
 
 # shell variables
-block=/dev/block/platform/soc/7824900.sdhci/by-name/boot;
-is_slot_device=1;
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=0;
 ramdisk_compression=auto;
 
 
@@ -25,38 +26,10 @@ ramdisk_compression=auto;
 # import patching functions/variables - see for reference
 . tools/ak3-core.sh;
 
-
-## AnyKernel file attributes
-# set permissions/ownership for included ramdisk files
-set_perm_recursive 0 0 755 644 $ramdisk/*;
-set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
-
-
 ## AnyKernel boot install
 dump_boot;
-
-# begin ramdisk changes
-
-
-
-# end ramdisk changes
 
 write_boot;
 ## end boot install
 
-
-# shell variables
-#block=vendor_boot;
-#is_slot_device=1;
-#ramdisk_compression=auto;
-
-# reset for vendor_boot patching
-#reset_ak;
-
-
-## AnyKernel vendor_boot install
-#split_boot; # skip unpack/repack ramdisk since we don't need vendor_ramdisk access
-
-#flash_boot;
-## end vendor_boot install
 
